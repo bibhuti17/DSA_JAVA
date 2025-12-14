afbe1a757419963eca0ebb9bea641aed91d3dd66
Bibhuti Bhusan Naik <126508100+bibhuti17@users.noreply.github.com>
2025-12-14 14:53:06 +0530

    some more code for collections

--- Problems_Mathematics/src/Prime_Number.java
+++ Problems_Mathematics/src/Prime_Number.java
@@ -1,2 +1,17 @@
 public class Prime_Number {
+    public static void main(String[] args) {
+        System.out.println(isPrime(2));
+
+    }
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return (c * c > n);
+    }
 }
